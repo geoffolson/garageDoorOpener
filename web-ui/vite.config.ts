4254a5b00d64f3f31f150c98b4245496c528d517
geoffolson <geoffrey.olson.jr@gmail.com>
2024-06-30 15:29:26 -0700

    add pwa feature

--- web-ui/vite.config.ts
+++ web-ui/vite.config.ts
@@ -1,9 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Garage Door",
+        short_name: "Garage",
+        description: "Garage door opener",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "remote.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "remote512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      registerType: "autoUpdate",
+    }),
+  ],
   server: {
     proxy: {
       "/api/": {
