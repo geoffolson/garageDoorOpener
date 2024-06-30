cf967328cbb79baf7821223a6bfaa8d4ea1d5ae9
geoffolson <geoffrey.olson.jr@gmail.com>
2024-06-30 00:16:44 -0700

    add code to Error

--- web-ui/src/Error.tsx
+++ web-ui/src/Error.tsx
@@ -12,6 +12,7 @@ export const ErrorAlert = ({
       className="m-auto bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative h-12 w-96"
       role="alert"
     >
+      <strong className="font-bold">{code}</strong>
       <span className="block sm:inline">{children}</span>
       <span
         className="absolute top-0 bottom-0 right-0 px-4 py-3"
