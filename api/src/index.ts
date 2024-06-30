7e302d1b6b39425f65fca6da108f4d18ceb1f666
geoffolson <geoffrey.olson.jr@gmail.com>
2024-06-30 00:42:13 -0700

    add static file hosting for web-ui

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express";
 import { pin } from "./pin";
+import path from "path";
 
 const app = express();
 const port = 3000;
+
+app.use(express.static(path.resolve(__dirname, "../../web-ui/dist")));
 
 app.post("/", async (req: Request, res: Response) => {
   await pin.press();
